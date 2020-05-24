0a403c2e9ad05194a2880bd0b253ad625e9a4a24
Josh Chough <joshchough@gmail.com>
2020-05-24 15:44:45 -0400

    Created restore method

--- backupOneTab.py
+++ backupOneTab.py
@@ -1,3 +1,4 @@
+import sys
 import os
 import shutil
 from datetime import datetime
@@ -6,31 +7,90 @@ chromedata_path = 'C:/Users/<Username>/AppData/Local/Google/Chrome/User Data/Def
 local_path = '/path/to/backups/folder/' # put your backups folder path here (make sure to include the last backslash /)
 exclude_file = 'LOCK' 
 
-# get current time
-now = datetime.now() # current date and time
-date_time = now.strftime('%m-%d-%Y_%H-%M-%S')
-print('date and time:', date_time)
+def backup():
+  # get current time
+  now = datetime.now() # current date and time
+  date_time = now.strftime('%m-%d-%Y_%H-%M-%S')
+  print('date and time:', date_time)
 
-# define src directory
-src = chromedata_path
+  # define src directory
+  src = chromedata_path
 
-# make dest_parent and dest directories
-dest_parent = local_path + date_time
-os.mkdir(dest_parent)
-print(dest_parent + ' created')
-dest = dest_parent + '/leveldb'
-os.mkdir(dest)
-print(dest + ' created')
+  # make dest_parent and dest directories
+  dest_parent = local_path + date_time
+  os.mkdir(dest_parent)
+  print(dest_parent + ' created')
+  dest = dest_parent + '/leveldb'
+  os.mkdir(dest)
+  print(dest + ' created')
 
-# copy ldb and log files from chromedata directory to local directory
-print('copying from ' + src + ' to ' + dest)
-src_files = os.listdir(src)
-for file_name in src_files:
-  if(file_name == exclude_file):
-    continue
-  full_file_name = os.path.join(src, file_name)
-  if os.path.isfile(full_file_name):
-    shutil.copy(full_file_name, dest)
-    print('copied ' + file_name)
+  # copy ldb and log files from chromedata directory to local directory
+  print('copying from ' + src + ' to ' + dest)
+  src_files = os.listdir(src)
+  for file_name in src_files:
+    if(file_name == exclude_file):
+      continue
+    full_file_name = os.path.join(src, file_name)
+    if os.path.isfile(full_file_name):
+      shutil.copy(full_file_name, dest)
+      print('copied ' + file_name)
+  
+  print('backup complete\n')
 
-print('backup complete\n')
+def restore(index):
+  # define dest directory
+  dest = chromedata_path
+
+  # define src directory
+  backups = next(os.walk(local_path))[1]
+  backups.reverse()
+  src = local_path + backups[index] + '/leveldb'
+
+  # getting file numbers from src
+  src_files = os.listdir(src)
+  src_files_compare = [file_name for file_name in src_files]
+
+  # remove files not in src from dest
+  print('removing files not in ' + src + ' from ' + dest)
+  dest_files = os.listdir(dest)
+  for file_name in dest_files:
+    if(file_name == exclude_file):
+      continue
+    full_file_name = os.path.join(dest, file_name)
+    if os.path.isfile(full_file_name) and file_name not in src_files_compare:
+      os.remove(full_file_name)
+      print('deleted ' + file_name)
+
+  # copy ldb and log files from local directory to chromedata directory
+  print('copying from ' + src + ' to ' + dest)
+  for file_name in src_files:
+    full_file_name = os.path.join(src, file_name)
+    if os.path.isfile(full_file_name):
+      shutil.copy(full_file_name, dest)
+      print('copied ' + file_name)
+  
+  print('restore complete\n')
+
+def main():
+  args = sys.argv[1:]
+
+  print()
+  if len(args) == 0:
+    fn = input('Backup or restore? ').lower()
+    print()
+    if fn == 'backup':
+      backup()
+    elif fn == 'restore':
+      index = int(input('Enter the backup ID: '))
+      restore(index)
+    else:
+      print('Error: Function not found')
+    print(input('Press \'Enter\' to exit'))
+  elif len(args) > 0 and args[0] == '--backup':
+    backup()
+  elif len(args) > 0 and args[0] == '--restore':
+    index = int(args[1])
+    restore(index)
+
+if __name__ == '__main__':
+  main()
